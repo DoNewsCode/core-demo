f4f9ef63635da1e7d57e4c9f8d20d52a855851b7
Reasno <guxi99@gmail.com>
2021-03-15 16:54:01 +0800

    fix: add grpc interceptor

--- cmd/internal/dependencies.go
+++ cmd/internal/dependencies.go
@@ -2,10 +2,13 @@ package internal
 
 import (
 	"github.com/DoNewsCode/core"
+	"github.com/DoNewsCode/core-kit/mw"
+	"github.com/DoNewsCode/core/di"
 	"github.com/DoNewsCode/core/observability"
 	"github.com/DoNewsCode/core/otgorm"
 	"github.com/DoNewsCode/core/otredis"
 	"github.com/DoNewsCode/core/ots3"
+	"google.golang.org/grpc"
 )
 
 func provide(c *core.C) {
@@ -13,4 +16,7 @@ func provide(c *core.C) {
 	c.Provide(otgorm.Providers())
 	c.Provide(otredis.Providers())
 	c.Provide(ots3.Providers())
+	c.Provide(di.Deps{func() *grpc.Server {
+		return grpc.NewServer(grpc.UnaryInterceptor(mw.Interceptor))
+	}})
 }
